7a1176424ac850c5bf24985cec1577cfc5047dae
Nima Dekhli <nima@dekhli.ch>
2024-03-13 10:06:28 +0100

    add chrono tool class to measure execution

--- src/ch/proco/objFilter/tools/Chrono.java
+++ src/ch/proco/objFilter/tools/Chrono.java
@@ -1,2 +1,28 @@
-package ch.proco.objFilter.tools;public class Chrono {
+package ch.proco.objFilter.tools;
+
+public class Chrono {
+    private long start = 0;
+    private long stop = 0;
+
+    public Chrono() {
+    }
+
+    public void start(){
+        assert start == 0;
+        this.start = System.nanoTime();
+    }
+
+    public void stop(){
+        assert stop == 0;
+        this.stop = System.nanoTime();
+    }
+
+    public void reset(){
+        this.start = 0;
+        this.stop = 0;
+    }
+
+    public double getEllapsedMillis(){
+        return (stop - start) / 1000000.0;
+    }
 }
